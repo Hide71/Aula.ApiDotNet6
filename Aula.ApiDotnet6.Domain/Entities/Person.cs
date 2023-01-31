8fbb92ad55b2b31c29611963c1a2eb1578e35daa
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2023-01-31 13:25:06 -0300

    atualizado 13hs

---
+++ Aula.ApiDotnet6.Domain/Entities/Person.cs
@@ -0,0 +1,38 @@
+﻿using Aula.ApiDotnet6.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.ApiDotnet6.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string name, string document, string phone )
+        {
+            Validation(name, document, phone);
+        }
+
+        public Person(int id, string name, string document, string phone)
+        {
+            DomainValidationException.When(id < 0, "ID inválido");
+            Id = id;
+            Validation(name, document, phone);
+        }
+        private void Validation(string name, string document, string phone)
+        {
+            DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado");
+            DomainValidationException.When(string.IsNullOrEmpty(phone), "Telefone deve ser informado");
+            Name = name;
+            Document = document;
+            Phone = phone;
+        }
+    }
+}
---
+++ Aula.ApiDotnet6.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula.ApiDotnet6.Domain.Validations
+{
+    public  class DomainValidationException:Exception
+    {
+        public DomainValidationException(string error) : base(error)
+        {
+
+        }
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+            {
+                throw new DomainValidationException(message);
+            }
+        }
+    }
+}
