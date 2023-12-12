0898f8d56dba09f45dc0a04297b15c7e7943bee8
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2023-12-12 14:55:59 -0300

    12122023

--- Aula.ApiDotnet6.Infra.Data/Repositories/PurchaseRepository.cs
+++ Aula.ApiDotnet6.Infra.Data/Repositories/PurchaseRepository.cs
@@ -2,7 +2,6 @@
 using Aula.ApiDotnet6.Domain.Repositories;
 using Aula.ApiDotnet6.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
